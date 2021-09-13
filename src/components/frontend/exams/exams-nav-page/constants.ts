6877bcc86adae8c09688a98994563a2f8cfe460e
unknown <proenglishxad@gmail.com>
2021-09-13 21:58:53 +0200

    update

--- src/components/frontend/exams/exams-nav-page/constants.ts
+++ src/components/frontend/exams/exams-nav-page/constants.ts
@@ -4,9 +4,9 @@ import sat from "public/exams/exams-nav-page/SAT.jpg";
 import gre from "public/exams/exams-nav-page/GRE.jpg";
 import gmat from "public/exams/exams-nav-page/GMAT.jpg";
 import bocconi from "public/exams/exams-nav-page/Bocconi.jpg";
-import fce from "public/exams/exams-nav-page/FCE.jpg";
-import cae from "public/exams/exams-nav-page/CAE.jpg";
-import cpe from "public/exams/exams-nav-page/CPE.jpg";
+// import fce from "public/exams/exams-nav-page/FCE.jpg";
+// import cae from "public/exams/exams-nav-page/CAE.jpg";
+// import cpe from "public/exams/exams-nav-page/CPE.jpg";
 
 export const links = [
   { name: "toefl", image: toefl, slug: "toefl" },
@@ -15,7 +15,7 @@ export const links = [
   { name: "gre", image: gre, slug: "gre" },
   { name: "gmat", image: gmat, slug: "gmat" },
   { name: "bocconi", image: bocconi, slug: "bocconi" },
-  { name: "fce - b2 first", image: fce, slug: "fce" },
-  { name: "cae - c1 advanced", image: cae, slug: "cae" },
-  { name: "CPE - C2 proficiency", image: cpe, slug: "CPE" },
+  // { name: "fce - b2 first", image: fce, slug: "fce" },
+  // { name: "cae - c1 advanced", image: cae, slug: "cae" },
+  // { name: "CPE - C2 proficiency", image: cpe, slug: "CPE" },
 ];
